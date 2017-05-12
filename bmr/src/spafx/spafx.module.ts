425bc84d1cb33000c52e0817a3c6e44909d095c6
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-05-12 13:00:51 -0500

    Adding the title-bar component in the spafx framework.  Want to update its name to SpafxTitleBar (consistent with the body).

--- bmr/src/spafx/spafx.module.ts
+++ bmr/src/spafx/spafx.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent} from './framework-body/framework-body.component';
 import { SpaFxContentComponent} from './spafx-content/spafx-content.component'; 
+import { TitleBarComponent} from './title-bar/title-bar.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { SpaFxContentComponent} from './spafx-content/spafx-content.component';
   ],
   declarations: [
     FrameworkBodyComponent,
-    SpaFxContentComponent
+    SpaFxContentComponent,
+    TitleBarComponent
   ],
   exports: [
     FrameworkBodyComponent
