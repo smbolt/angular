b5976327c804225d586a4bf6d1adf15024dca3ba
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-05-14 11:56:11 -0500

    Adding spafx-top-bar component.

---
+++ bmr/src/spafx/spafx-top-bar/spafx-top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spafx-top-bar',
+  templateUrl: './spafx-top-bar.component.html',
+  styleUrls: ['./spafx-top-bar.component.css']
+})
+export class SpafxTopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- bmr/src/spafx/spafx.module.ts
+++ bmr/src/spafx/spafx.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent} from './framework-body/framework-body.component';
 import { SpaFxContentComponent} from './spafx-content/spafx-content.component'; 
 import { TitleBarComponent} from './title-bar/title-bar.component';
+import { SpafxTopBarComponent } from './spafx-top-bar/spafx-top-bar.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { TitleBarComponent} from './title-bar/title-bar.component';
   declarations: [
     FrameworkBodyComponent,
     SpaFxContentComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    SpafxTopBarComponent
   ],
   exports: [
     FrameworkBodyComponent
