ff1a5d3f0dbfaa773d8ebc3b50b67bb7826d9f05
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-05-17 12:21:25 -0500

    Adding the spafx-status-bar code files.

---
+++ bmr/src/spafx/spafx-status-bar/spafx-status-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spafx-status-bar',
+  templateUrl: './spafx-status-bar.component.html',
+  styleUrls: ['./spafx-status-bar.component.css']
+})
+export class SpafxStatusBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
