c162dbf16bebb4193c8a45d86e07267cc8fd4472
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-05-25 12:46:18 -0500

    Adding directives for detecting screen size.

--- bmr/src/spafx/spafx.module.ts
+++ bmr/src/spafx/spafx.module.ts
@@ -7,6 +7,8 @@ import { SpaFxContentComponent} from './spafx-content/spafx-content.component';
 import { TitleBarComponent} from './title-bar/title-bar.component';
 import { SpafxTopBarComponent } from './spafx-top-bar/spafx-top-bar.component';
 import { SpafxStatusBarComponent } from './spafx-status-bar/spafx-status-bar.component';
+import { ScreenLarge } from './directives/screen-large.directive';
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
 
 @NgModule({
   imports: [
@@ -17,7 +19,9 @@ import { SpafxStatusBarComponent } from './spafx-status-bar/spafx-status-bar.com
     SpaFxContentComponent,
     TitleBarComponent,
     SpafxTopBarComponent,
-    SpafxStatusBarComponent
+    SpafxStatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
   ],
   providers:[
     FrameworkConfigService,
