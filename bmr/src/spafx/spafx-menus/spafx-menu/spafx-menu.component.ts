4a3dc24dc80df315cbfff9ce14dd0adca3f7a955
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-06-14 12:48:18 -0500

    Adding spa-fx menu related files.

---
+++ bmr/src/spafx/spafx-menus/spafx-menu/spafx-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { SpaFxMenuService } from '../../services/spafx-menu.service';
+
+@Component({
+  selector: 'spafx-menu',
+  templateUrl: './spafx-menu.component.html',
+  styleUrls: ['./spafx-menu.component.css']
+})
+export class SpaFxMenuComponent implements OnInit {
+
+  constructor(private menuService: SpaFxMenuService) { }
+
+  ngOnInit() {
+  }
+
+}
