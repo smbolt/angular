d147aa4367a81106a9af0a7e1d0c2848d8568662
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-06-21 12:43:24 -0500

    Adding ability to toggle menu orientation via buttons in the UI.

--- bmr/src/spafx/services/spafx-menu.service.ts
+++ bmr/src/spafx/services/spafx-menu.service.ts
@@ -15,6 +15,11 @@ export class SpaFxMenuService {
   showingLeftSideMenu = false;
 
   toggleLeftSideMenu() : void {
+    this.isVertical = true;
     this.showingLeftSideMenu = !this.showingLeftSideMenu;
+  }
+
+  toggleOrientation() {
+    this.isVertical = !this.isVertical;
   }
 }
