8cbf3f742272707dd43645ee0aaf6cd6237544b5
Stephen Bolt on NB-98 <stephen.m.bolt@gmail.com>
2017-07-10 12:39:50 -0500

    Adding some files related to last commit.

---
+++ bmr/src/spafx/spafx-menus/spafx-popup-menu/spafx-popup-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'spafx-popup-menu',
+  templateUrl: './spafx-popup-menu.component.html',
+  styleUrls: ['./spafx-popup-menu.component.css']
+})
+export class SpafxPopupMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
