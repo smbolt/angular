40231930a6d9a164cf85eeb64f5f424525545236
Stephen Bolt on NB-99 <stephen.m.bolt@gmail.com>
2017-11-15 20:19:52 -0600

    Fixing up some merge conflicts.

--- bmr/e2e/app.po.ts
+++ bmr/e2e/app.po.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { browser, element, by } from 'protractor';
-=======
-import { browser, by, element } from 'protractor';
->>>>>>> 0c1ab07e3b10908a3c77a070b1f6f17c53c92660
 
 export class BmrPage {
   navigateTo() {
